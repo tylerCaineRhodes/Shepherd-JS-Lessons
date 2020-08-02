d444b40ec2cc1b6583e7e1a709ebd8f625cb298a
Brian McHugh <125cedarstreet@gmail.com>
2020-08-02 17:34:30 -0500

    Add Problem #6 to thisCheckpoint.js

--- Week 2/thisCheckpoint.js
+++ Week 2/thisCheckpoint.js
@@ -100,3 +100,21 @@ console.log(result4);
 
 var result5 = jenny.greet.call(bobby);
 console.log(result5);
+
+// -----------------------------------------
+// Problem 6
+
+// what should we see if we call getArea() on line 122?
+
+const circle = {
+  radius: 10,
+  color: 'orange',
+  getArea: function() {
+    return Math.PI * this.radius * this.radius;
+  },
+  getCircumference: function() {
+    return 2 * Math.PI * this.radius;
+  }
+};
+
+let {radius, getArea, getCircumference} = circle;
